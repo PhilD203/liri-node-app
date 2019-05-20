7789ad04dc131557b4d877a5f05dfc2c8c60a78f
Phil Dugliss <phillipdugliss@gmail.com>
2019-05-19 19:38:03 -0500

    Refactored for functionality
    Functions added Spotify this, concert this, movie this, Do-what-it- says working

--- index.js
+++ index.js
@@ -8,6 +8,49 @@ let fs = require("fs");
 let Spotify = require('node-spotify-api');
 let spotify = new Spotify(keys.spotify);
 
+function spotifyThis(query){
+    spotify.search({ type: 'track', query: query, limit: 1 }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        let trackResult = data.tracks.items[0]
+        console.log(`
+            Artist: ${trackResult.artists[0].name}
+            Album: ${trackResult.album.name}
+            Released: ${trackResult.album.release_date}
+            Link: ${trackResult.album.href} `)
+    });
+}
+
+function concertThis(query){
+    
+    var queryBands = "https://rest.bandsintown.com/artists/" + query + "/events?app_id=codingbootcamp";
+
+    axios.get(queryBands).then(
+        function(bandResponse){   
+            
+            console.log(`
+            Venue: ${bandResponse.data[0].venue.name}
+            City:  ${bandResponse.data[0].venue.city}
+            Date: ${(moment(bandResponse.data[0].datetime).format("MM/DD/YYYY"))}
+            `)
+        })        
+}
+
+function movieThis(query){
+    var movieQuery = "https://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy";
+
+        axios.get(movieQuery).then(function(movieResponse){
+
+                                console.log(`
+                                Year: ${movieResponse.data.Year}
+                                Rated: ${movieResponse.data.imdbRating}
+                                Actors: ${movieResponse.data.Actors}
+                                Plot: ${movieResponse.data.Plot}
+                                `)
+                            })   
+}
+
 figlet('The Entertainer', function (err, data) {
     if (err) {
         console.log('Something went wrong...');
@@ -19,7 +62,7 @@ figlet('The Entertainer', function (err, data) {
     inquirer.prompt([
         {
             type: "list",
-            message: "WHAT DO YOU WANT?",
+            message: "WHAT WOULD YOU LIKE TO DO?",
             choices: ["SPOTIFY-THIS-SONG", "CONCERT-THIS", "MOVIE-THIS", "DO-WHAT-IT-SAYS"],
             name: "user_choice"
         },
@@ -33,17 +76,7 @@ figlet('The Entertainer', function (err, data) {
                         message: "Which song would you want to hear?",
                         name: "song_name"
                     }]).then(function (response) {
-                        spotify.search({ type: 'track', query: response.song_name, limit: 1 }, function (err, data) {
-                            if (err) {
-                                return console.log('Error occurred: ' + err);
-                            }
-                            let trackResult = data.tracks.items[0]
-                            console.log(`
-                                Artist: ${trackResult.artists[0].name}
-                                Album: ${trackResult.album.name}
-                                Released: ${trackResult.album.release_date}
-                                Link: ${trackResult.album.href} `)
-                        });
+                       spotifyThis(response.song_name);
                     })
             }
             else if (inquirerResponse.user_choice === "CONCERT-THIS") {
@@ -54,18 +87,7 @@ figlet('The Entertainer', function (err, data) {
                             message: "Which show would you want to see?",
                             name: "show_name"
                         }]).then(function (response) {
-
-                            var queryBands = "https://rest.bandsintown.com/artists/" + response.show_name + "/events?app_id=codingbootcamp";
-
-                            axios.get(queryBands).then(
-                                function(bandResponse){   
-                                    
-                                    console.log(`
-                                    Venue: ${bandResponse.data[0].venue.name}
-                                    City:  ${bandResponse.data[0].venue.city}
-                                    Date: ${(moment(bandResponse.data[0].datetime).format("MM/DD/YYYY"))}
-                                    `)
-                                })        
+                            concertThis(response.show_name);
                     })
             }
 
@@ -78,36 +100,35 @@ figlet('The Entertainer', function (err, data) {
                             name: "movie_name"
                         }]).then(function (response) {
 
-                            var movieQuery = "https://www.omdbapi.com/?t=" + response.movie_name + "&y=&plot=short&apikey=trilogy";
-
-                            axios.get(movieQuery).then(function(movieResponse){
-
-                                console.log(`
-                                Year: ${movieResponse.data.Year}
-                                Rated: ${movieResponse.data.imdbRating}
-                                Actors: ${movieResponse.data.Actors}
-                                Plot: ${movieResponse.data.Plot}
-                                `)
-                            })                             
+                            movieThis(response.movie_name);                          
                         })
                         
                 }
 
             else if (inquirerResponse.user_choice === "DO-WHAT-IT-SAYS") {
-                function entertainerSays() {
+                   
 
-
-                    fs.readFile("random.text", "utf8", function(error, data) {
+                    fs.readFile("random.txt", "utf8", function(error, data) {
                       if (error) {
                         return console.log(error);
                       }
                         var output = data.split(",");
-                        for (var i = 0; i < output.length; i++) {
-                            console.log(output[i]);
-                        }
+                        var random = Math.floor(Math.random() * (output.length / 2)); 
+
+                        console.log(output[random * 2]);
+
+                       if (output[random * 2] === "MOVIE-THIS"){
+                           movieThis(output[(random * 2) + 1]);
+                       }
+                       else if(output[random * 2] === "SPOTIFY-THIS"){
+                        spotifyThis(output[(random * 2) + 1]);
+                       }
+                       else if(output[random * 2] === "CONCERT-THIS"){
+                        concertThis(output[(random * 2) + 1]);
+                       }
                       });
-                };
-                entertainerSays();
+                
+              
             }
 
     });
